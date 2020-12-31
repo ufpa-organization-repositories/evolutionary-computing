8874da1919e3ae0f1b107c91de8685089f5d148b
brunoconde07 <brunoconde.ufpa@gmail.com>
2020-12-31 17:57:08 -0300

    homework 04

---
+++ homework_04/calc_pop.py
@@ -0,0 +1,20 @@
+def calc_pop(len_pop, x_inicial, x_final, y_inicial, y_final, i_experimento):
+    from random import uniform, seed, random
+    from to_binary import to_binary
+    from calc_fitness import calc_fitness
+    populacao = []
+
+    for i in range(len_pop):
+        seed(i * i_experimento)
+        x = to_binary(float("%.5f" % uniform(x_inicial, x_final)))
+        y = to_binary(float("%.5f" % uniform(y_inicial, y_final)))
+        populacao.append([x, y])
+
+    # calc_fitness(pop=populacao)
+    calc_fitness(pop=populacao)
+    # print(len(populacao), populacao)
+    return populacao
+
+# len_pop = 100; x_inicial = -5; x_final = 5; y_inicial = -5; y_final = 5; populacao = []
+# calc_pop()
+
